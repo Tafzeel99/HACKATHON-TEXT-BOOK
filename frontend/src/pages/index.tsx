109da17cef0b9516376dbbef4babc2088b587c7e
Tafzeel Ahmed Khan <roshan.king2244@gmail.com>
2025-12-17 03:45:42 +0500

    feat: Dynamically update meta description on homepage

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -34,7 +34,7 @@ export default function Home(): ReactNode {
   return (
     <Layout
       title={`${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
+      description={`${siteConfig.title} - ${siteConfig.tagline}`}>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
